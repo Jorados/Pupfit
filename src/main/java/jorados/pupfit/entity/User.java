cecb79fc2cc6129514965009e6ae0d6d1ad8c0aa
seongjin8860 <seongjin8860@naver.com>
2024-07-27 22:44:22 +0900

    feat : Swagger API 문서 적용 ( 자세히 x )

--- src/main/java/jorados/pupfit/entity/User.java
+++ src/main/java/jorados/pupfit/entity/User.java
@@ -33,6 +33,7 @@ public class User {
     private String username; // 시큐리티 로그인 ID
 
     @Column(nullable = false, length = 60)
+//    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}", message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     private String password; // 암호
 
     @Column(length = 20)
